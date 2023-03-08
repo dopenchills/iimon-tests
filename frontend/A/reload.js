846ea38b9c1c3529095c59d7c7a014031d4a5770
dopenchills <dopenchills@gmail.com>
2023-03-08 19:34:45 +0900

    feat: add frontend/A (only for normal flow)

---
+++ frontend/A/reload.js
@@ -0,0 +1,18 @@
+const reload = () => {
+  const endpoint      = "https://catfact.ninja/fact";
+  const text_element  = document.querySelector("p.text");
+  fetch(endpoint, {
+    method: "GET"
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    else {
+      console.error(`API call failed: ${endpoint}`);
+    }
+  }).then(json => {
+    const response = json;
+    text_element.textContent = response.fact;
+    console.log(response);
+  })
+};
