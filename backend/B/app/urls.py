e843e294259fb21233ba571b8c89e6c07ad76dbf
dopenchills <dopenchills@gmail.com>
2023-03-13 20:12:14 +0900

    feat: add routing

--- backend/B/app/urls.py
+++ backend/B/app/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('login/', views.login, name='login'),
+    path('signin/', views.signin, name='signin'),
+    path('posts/', views.posts, name="posts")
 ]
