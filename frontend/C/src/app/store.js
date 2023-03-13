b5acbb314c424ce1c3046ba008bc592aad8e3d25
dopenchills <dopenchills@gmail.com>
2023-03-13 13:21:06 +0900

    fix: delete unnecessary Counter component

--- frontend/C/src/app/store.js
+++ frontend/C/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import fruitsReducer from '../features/friuts/fruitsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     fruits: fruitsReducer,
   },
 });
